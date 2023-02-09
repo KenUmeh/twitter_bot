414a79053944d176eb5ccd868dfc87e9733d6afa
Ekene <ekeneumeh13@gmail.com>
2023-02-09 16:49:10 +0100

    new commit

--- filetest.js
+++ filetest.js
@@ -8,57 +8,28 @@ const pdf = require('pdf-parse')
 //     console.log(chunk.toString())
 // });
 
-// const getPdf = async(file) => {
-//     let readFileSync = fs.readFileSync(file)
-//   try {
-//     let pdfExtract = await pdf(readFileSync)
-//     let content = pdfExtract.text
+const getPdf = async(file) => {
+    let readFileSync = fs.readFileSync(file)
+  try {
+    let pdfExtract = await pdf(readFileSync)
+    let content = pdfExtract.text
     
-//     // let pages=content.split("\n")
-//     // pages.forEach((e, i)=>{
-//     //   console.log(i)
-//     // })
+    // let pages=content.split("\n")
+    // pages.forEach((e, i)=>{
+    //   console.log(i)
+    // })
     
-//     console.log(content)
-//     console.log('Total pages: ', pdfExtract.numpages)
-//     console.log('All content: ', pdfExtract.info)
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
-
-// const pdfread = './The_Daily_Stoic_366.pdf'
-// getPdf(pdfread)
-
-function render_page(pageData) {
-  //check documents https://mozilla.github.io/pdf.js/
-  let render_options = {
-      //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
-      normalizeWhitespace: false,
-      //do not attempt to combine same line TextItem's. The default value is `false`.
-      disableCombineTextItems: false
+    console.log(content)
+    console.log('Total pages: ', pdfExtract.numpages)
+    console.log('All content: ', pdfExtract.info)
+  } catch (error) {
+    console.log(error)
   }
-
-  return pageData.getTextContent(render_options)
-  .then(function(textContent) {
-      let lastY= '';
-      let text=[]
-      for (let item of textContent.items) {
-       text.push(item)
-      }
-      return text;
-  });
 }
 
-let options = {
-  pagerender: render_page
-}
+const pdfread = './public/doc/The_Daily_Stoic_366.pdf'
+getPdf(pdfread)
 
-let dataBuffer = fs.readFileSync('./public/doc/The_Daily_Stoic_366.pdf');
 
-pdf(dataBuffer,options).then(function(data) {
-  //use new format
-  console.log(data)
-});
 
 
--- twitterClient.js
+++ twitterClient.js
@@ -2,7 +2,12 @@ const twitterApi = require("twitter-api-v2").default
 
 //console.log(twitterApi)
 
-
+const client = new twitterApi({
+    appKey: "euGvYNutOr3zrl391tbZ2EMfd",
+    appSecret: "HOdKUt0oCOalJVaxdyV6ElHVncrHpeiLZ0NFOhfu9ayaxts5QC",
+    accessToken: "883993107171356673-WSxTBo7aTM2jQBrArTypz6o4JJKsxtZ",
+    accessSecret: "5RepuMLBjbstuKNI6tqB6l9tbdUncVas1Ru6GKYK0G20x"
+})
 
 
 const rwClient = client.readWrite
