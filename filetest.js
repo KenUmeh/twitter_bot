2ceb2fd51520b73cd2c3f05ad6d7864f57fb3796
Ekene <ekeneumeh13@gmail.com>
2023-03-04 12:24:10 +0100

    content splitting and thread creation

--- filetest.js
+++ filetest.js
@@ -8,7 +8,7 @@ var d = moment();
 var date = d.format("MMMM Do");
 console.log("this is today's date", date);
 
-var tweetContent;
+// var tweetContent;
 
 const loadPage = async function (pageNum, doc) {
   let page = await doc.getPage(pageNum);
@@ -19,65 +19,13 @@ const loadPage = async function (pageNum, doc) {
   return strings.join(" ");
 };
 const pdfProcess = async () => {
-  /*
-  approach one even though it looks too dirty for my liking though
-  */
-  // return await loadingTask.promise
-  //   .then(async function (doc) {
-  //     const numPages = doc.numPages;
-  //     // console.log("# Document Loaded");
-  //     // console.log("Number of Pages: " + numPages);
-  //     // console.log();
-  //     // let lastPromise; // will be used to chain promises
-  //     // lastPromise = doc.getMetadata().then(function (data) {
-  //     // console.log("# Metadata Is Loaded");
-  //     // console.log("## Info");
-  //     // console.log(JSON.stringify(data.info, null, 2));
-  //     // console.log();
-  //     // if (data.metadata) {
-  //     // console.log("## Metadata");
-  //     // console.log(JSON.stringify(data.metadata.getAll(), null, 2));
-  //     // console.log();
-  //     // }
-  //     // });
-  //     const loadPage = async function (pageNum) {
-  //       let page = await doc.getPage(pageNum);
-  //       let content = await page.getTextContent();
-  //       const strings = content.items.map(function (item) {
-  //         return item.str;
-  //       });
-  //       return strings.join(" ");
-  //     };
-  //     for (let i = 1; i <= numPages; i++) {
-  //       let pageData = await loadPage(i);
-  //       if (pageData.includes(date)) {
-  //         tweetContent = pageData;
-  //         console.log(pageData);
-  //         return pageData;
-  //         //console.log(tweetContent);
-  //         //  console.log("tweet length", tweetContent.length);
-  //         // console.log("page " + i);
-  //         return tweetContent;
-  //       }
-  //       // loadPage(i).then((d) => {
-  //       //   if(d.includes(date)){
-  //       //      tweetContent = d
-  //       //     console.log(tweetContent)
-  //       //     console.log("tweet length", tweetContent.length)
-  //       //   // console.log("page " + i);
-  //       //   }
-  //       // });
-  //     }
-  //   })
-  //   .then(
-  //     function () {
-  //       console.log("# End of Document");
-  //     },
-  //     function (err) {
-  //       console.error("Error: " + err);
-  //     }
-  //   );
 
+
+  /*
+  this is the second implimentation which is alot concise and effective
+  it will be my favourite
+  */
+  
   /*
   this is the second implimentation which is alot concise and effective
   it will be my favourite
--- index.js
+++ index.js
@@ -1,21 +1,100 @@
 const rwClient = require("./twitterClient")
 const cronJob = require("cron").CronJob
-const content = require("./filetest")
 
-console.log("the tweet content", content)
+const pdfLibjs = require("pdfjs-dist/legacy/build/pdf.js");
+const moment = require("moment");
 
-const tweet = async() => {
+const loadingTask = pdfLibjs.getDocument(
+  "./public/doc/The_Daily_Stoic_366.pdf"
+);
+var d = moment();
+var date = d.format("MMMM Do");
+console.log("this is today's date", date);
+
+// var tweetContent;
+
+const loadPage = async function (pageNum, doc) {
+  let page = await doc.getPage(pageNum);
+  let content = await page.getTextContent();
+  const strings = content.items.map(function (item) {
+    return item.str;
+  });
+  return strings.join(" ");
+};
+const pdfProcess = async () => {
+  
+  let doc = await loadingTask.promise;
+  const numPages = doc.numPages;
+  for (let i = 1; i <= numPages; i++) {
+    let pageData = await loadPage(i, doc);
+    if (pageData.includes(date)) {
+      return pageData;
+    }
+  }
+};
+
+//this is a promise until you can call it in an async function with an await which will
+// resolve the promise authomatically else you will get null since javascript is compiled
+// synchronously unless you take your remote and be in charge.
+pdfProcess()
+  .then((r) => {
+    console.log('tweet content  ', r);
+    console.log('tweet length  ', r.length);
+    const job = new cronJob("* * * * *", () => {
+        console.log("test cron job")
+        tweet(r)
+    
+    })
+     job.start();
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const tweet = async(str) => {
     try {
-        await rwClient.v1.tweet("Bienvenue mon peuple")
+            //  let snd = 'stand with the philosopher, or else with the mob!”   — E PICTETUS,   D ISCOURSES , 3.15.13  e’re all complicated people. We have multiple sides to ourselves—conflicting wants, desires, and fears. The outside world is no less confusing and contradictory. If we’re not careful, all '
+            // let id = '1631563811046948864'
+            // let id_str= '883993107171356673'
+            // let resp = await rwClient.v1.tweet(snd)
+              // let resp = await rwClient.v1.reply(snd, id)
+            //  console.log(resp)
+
+
+          const chunkSize = 280; // set the maximum size of each chunk
+          const result = [];
+          
+          for (let i = 0; i < str.length; i += chunkSize) {
+            const chunk = str.slice(i, i + chunkSize); // slice the string into chunks
+            if (chunk.length > chunkSize) {
+              throw new Error(`Cannot split string into chunks of ${chunkSize} characters or less.`);
+            }
+            result.push(chunk);
+          }
+          console.log(result)
+          console.log("number of chunks", result.length)
+          let l = result.length
+          var tID 
+          for (let i = 0; i < l; i++){
+             if(i === 0){
+              console.log('first tweet content', result[i])
+              let resp = await rwClient.v1.tweet(result[i])
+             console.log("first tweet response", resp)
+               tID = resp.id_str
+             }else{
+              console.log('response tweet', i)
+             let resp = await rwClient.v1.reply(result[i], tID)
+             console.log('response tweet', resp)
+              tID = resp.id_str
+             }
+
+          }
+          return result;
+
+        
     } catch (e) {
         console.log(e)
     }
 }
 
-const job = new cronJob("0 6 * * *", () => {
-    console.log("test cron job")
-    tweet()
-
-})
- job.start();
 // tweet()
