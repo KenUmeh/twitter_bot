3eee2461f31d392e967e8158a25398845a7f5649
Ekene <ekeneumeh13@gmail.com>
2023-03-24 17:26:46 +0100

    new commit

--- twitterClient.js
+++ twitterClient.js
@@ -3,11 +3,14 @@ const twitterApi = require("twitter-api-v2").default
 //console.log(twitterApi)
 
 const client = new twitterApi({
-    appKey: "euGvYNutOr3zrl391tbZ2EMfd",
-    appSecret: "HOdKUt0oCOalJVaxdyV6ElHVncrHpeiLZ0NFOhfu9ayaxts5QC",
+    appKey: CONSUMER_KEY,
+    appSecret: CONSUMER_SECRET,
     accessToken: "883993107171356673-WSxTBo7aTM2jQBrArTypz6o4JJKsxtZ",
     accessSecret: "5RepuMLBjbstuKNI6tqB6l9tbdUncVas1Ru6GKYK0G20x"
 })
+
+// appKey: "euGvYNutOr3zrl391tbZ2EMfd",
+// appSecret: "HOdKUt0oCOalJVaxdyV6ElHVncrHpeiLZ0NFOhfu9ayaxts5QC",
 
 
 const rwClient = client.readWrite
